2c8bd2488b691f601814349ffc63d07dd82265c2
Akash Kotla <akashkotla1999@gmail.com>
2022-03-16 15:25:24 +0530

    source code

---
+++ script.js
@@ -0,0 +1,12 @@
+let search=document.querySelector('.search')
+let btn=document.querySelector('.btn')
+let input=document.querySelector('.input')
+btn.addEventListener('click',() =>{
+    search.classList.toggle('active')
+    
+
+
+    input.focus()
+
+
+})
